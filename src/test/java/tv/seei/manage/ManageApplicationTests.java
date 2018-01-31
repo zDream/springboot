d5b8ce8bca9fb342e3d36a24854fd40861ebd439
drayy <zttongdeyx@126.com>
2018-01-31 16:45:29 +0800

    ManageApplicationTests

--- src/test/java/tv/seei/manage/ManageApplicationTests.java
+++ src/test/java/tv/seei/manage/ManageApplicationTests.java
@@ -1,6 +1,5 @@
 package tv.seei.manage;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-import tv.seei.manage.authorization.dao.TestUserDao;
 import tv.seei.manage.authorization.dao.UserDao;
-import tv.seei.manage.authorization.entity.TestUser;
-import tv.seei.manage.authorization.entity.TokenModel;
 import tv.seei.manage.authorization.entity.User;
 
 import java.util.concurrent.TimeUnit;
@@ -23,9 +19,6 @@ public class ManageApplicationTests {
 
 	@Autowired
 	private UserDao userDao;
-
-	@Autowired
-	private TestUserDao testUserDao;
 
 	@Autowired
 	private RedisTemplate redisTemplate;
